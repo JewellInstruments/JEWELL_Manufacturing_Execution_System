f84ccf6babe26db1f70b5de9255302324b8fce58
ngreen01 <ngreen@jewellinstruments.com>
2024-10-22 11:32:10 -0400

    fix bat file

--- RUBY_BON.bat
+++ RUBY_BON.bat
@@ -1,4 +1,4 @@
-git pull https://github.com/JewellInstruments/RUBY_BON_Program.git main
+git pull https://github.com/JewellInstruments/JEWELL_Manufacturing_Execution_System.git main
 call ".venv\Scripts\activate.bat"
 @REM pip install -U -r requirements.txt
 py main.py
